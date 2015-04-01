43edfb058dae111beadc671303326d7a301381ed
Godot-Arrives <godot.arrives@gmail.com>
2015-04-01 18:04:54 -0500

    EraseAllSectors Working
    Made some fixes, compiled and tested first function. Appears to be
    working as intended.

--- driverlayer.h
+++ driverlayer.h
@@ -4,11 +4,15 @@
 
 void EraseAllSectors()
 {
-	HANDLE memory = CreateFile( "memory.dat",
+	DWORD dwBytesWritten = 0;
+
+	HANDLE memory = CreateFile( "memory.bin",
 				(GENERIC_READ | GENERIC_WRITE),
 				0,
+				NULL,
 				OPEN_ALWAYS,
-				FILE_ATTRIBUTE_NORMAL);
+				FILE_ATTRIBUTE_NORMAL,
+				NULL);
 
 	LPVOID freshMemorySector = HeapAlloc(GetProcessHeap(),
 					HEAP_GENERATE_EXCEPTIONS,
@@ -16,27 +20,33 @@ void EraseAllSectors()
 
 	FillMemory(freshMemorySector,
 		nFileSizeBytes/20,
-		0xFF);
+		0xff);
 
-	for(int sect = 0; sect < 20; sect++)
-	{
+	for(int sect = 0; sect < 20; sect++){
+	
 		WriteFile( memory,
 			freshMemorySector,
-			nFileSizeBytes/20);
+			nFileSizeBytes/20,
+			&dwBytesWritten,
+			NULL);
 	}
 
 	HeapFree(GetProcessHeap(),
 		0,
 		freshMemorySector);
+
+	CloseHandle(memory);
 }
 
 void EraseSector(int nSectorNr)
 {
-	HANDLE memory = CreateFile( "memory.dat",
-				GENERIC_WRITE,
+	HANDLE memory = CreateFile(  "memory.bin",
+				(GENERIC_READ | GENERIC_WRITE),
 				0,
+				NULL,
 				OPEN_ALWAYS,
-				FILE_ATTRIBUTE_NORMAL);
+				FILE_ATTRIBUTE_NORMAL,
+				NULL);
 
 	LPVOID freshMemorySector = HeapAlloc(GetProcessHeap(),
 					HEAP_GENERATE_EXCEPTIONS,
@@ -46,24 +56,34 @@ void EraseSector(int nSectorNr)
 		nFileSizeBytes/20,
 		0xFF);
 
+	CloseHandle(memory);
+	
 }
 
 void ReadWord(int nAddress)
 {
-	HANDLE memory = CreateFile( "memory.dat",
-			GENERIC_WRITE,
-			0,
-			OPEN_ALWAYS,
-			FILE_ATTRIBUTE_NORMAL);
+	HANDLE memory = CreateFile( "memory.bin",
+				(GENERIC_READ | GENERIC_WRITE),
+				0,
+				NULL,
+				OPEN_ALWAYS,
+				FILE_ATTRIBUTE_NORMAL,
+				NULL);
 
+	CloseHandle(memory);
+	
 }
 
 void WriteWord(int nAddress, int nWord)
 {
-	HANDLE memory = CreateFile( "memory.dat",
-			GENERIC_WRITE,
-			0,
-			OPEN_ALWAYS,
-			FILE_ATTRIBUTE_NORMAL);
+	HANDLE memory = CreateFile( "memory.bin",
+				(GENERIC_READ | GENERIC_WRITE),
+				0,
+				NULL,
+				OPEN_ALWAYS,
+				FILE_ATTRIBUTE_NORMAL,
+				NULL);
 
+	CloseHandle(memory);
+	
 }
--- testingMain.cpp
+++ testingMain.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
+	EraseAllSectors();
 	return 0;
 }
